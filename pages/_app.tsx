748db85c5ef378b211c1b64a24dab76f2fab926e
Paul Lorenc <pjlorenc@gmail.com>
2020-12-29 18:01:14 -0500

    added seo to boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps /*, AppContext */ } from "next/app";
 import Head from "next/head";
 import "../styles/main.css";
+import { DefaultSeo } from "next-seo";
+import SEO from "../next-seo.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
   );
